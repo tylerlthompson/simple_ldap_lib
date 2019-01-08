5a9c5927f69f12e033f20347b6f1e6ba918acdee
Brandon Rodriguez <brodriguez8774@gmail.com>
2019-01-08 16:32:15 -0500

    Create auth_with_uid function

--- main.py
+++ main.py
@@ -21,22 +21,26 @@ if __name__ == "__main__":
     ldap_lib = simple_ldap_lib.SimpleLdap()
 
     # # Get username and password from console prompt.
-    # username = input('Ldap DN: ')
-    # password = getpass('Password: ')
+    username = input('Ldap DN: ')
+    password = getpass('Password: ')
 
-    # Attempt authentication.
-    results = ldap_lib.authenticate()
-    logger.info('Authentication Results: {0}'.format(results))
+    # # Attempt authentication.
+    # results = ldap_lib.authenticate()
+    # logger.info('Authentication Results: {0}'.format(results))
+    #
+    # # Connect to server.
+    # ldap_lib.bind_server()
+    # # ldap_lib.bind_server(dn=username, password=password)
+    #
+    # # Search for values.
+    # logger.info('')
+    # logger.info('Attempting search...')
+    # results = ldap_lib.search()
+    # logger.info('Search results: {0}'.format(results))
 
-    # Connect to server.
-    ldap_lib.bind_server()
-    # ldap_lib.bind_server(dn=username, password=password)
-
-    # Search for values.
-    logger.info('')
-    logger.info('Attempting search...')
-    results = ldap_lib.search()
-    logger.info('Search results: {0}'.format(results))
+    # Attempt authentication with uid.
+    results = ldap_lib.authenticate_with_uid(username, password)
+    logger.info('Auth with UID Results: {0}'.format(results))
 
     # Close program.
     logger.info('')
--- resources/config.py
+++ resources/config.py
@@ -1,11 +1,11 @@
 
 config = {
-    'host': '',
-    'dn': '',
-    'password': '',
+    'host': 'ldaps://',
+    'master_dn': '',
+    'master_password': '',
     'search_base': '',
-    'search_filter': '',
-    'attributes': [],
+    'search_filter': '(uid={0})',
+    'attributes': ['cn',],
     'connection_timeout': '30',
     'search_timeout': '300',
 }
